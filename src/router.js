a5c7730db68bac76b08c730de016c30b6779de93
Austin Caron <aus10caron@icloud.com>
2019-06-04 20:40:52 -0400

    Setup router

--- src/router.js
+++ src/router.js
@@ -1,25 +1,17 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import { IonicVueRouter } from "@ionic/vue";
+import Home from "./views/Home.vue";
 
-Vue.use(Router)
+Vue.use(IonicVueRouter);
 
-export default new Router({
-  mode: 'history',
+export default new IonicVueRouter({
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
-})
+});
