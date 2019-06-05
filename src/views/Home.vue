989e267d3f7aa002d46fd621e9b3d8b844b96706
Austin Caron <aus10caron@icloud.com>
2019-06-04 20:42:30 -0400

    Setup initial  markup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>ZipInfo</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <ZipSearch/>
+    </ion-content>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ZipSearch from "../components/ZipSearch";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home",
+  components: { ZipSearch }
+};
 </script>
